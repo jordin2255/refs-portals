fdd7f36a79ecd1fdf91af94ac0f9fb733b00f5d5
jordin <jordinmathew.mec@gmail.com>
2024-10-07 08:40:30 +1000

    useref hook

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,16 @@
+import {useState, useRef} from 'react';
 export default function Player() {
+  const playerName=useRef();
+  const [inputPlayerName,setinputPlayerName]=useState(null);
+  function handleClick(){
+    setinputPlayerName(playerName.current.value);
+  }
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {inputPlayerName ?? 'unknown entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input ref={playerName} type="text" />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
